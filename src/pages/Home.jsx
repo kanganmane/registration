a3dd29abde6decf4833d0d2bdeca4108611b4642
Kangan Mane <kanganmane16@gmail.com>
2026-04-08 21:29:08 +0530

    more light theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' w-full h-full flex items-center text-4xl  justify-center bg-[#161616]'>Home</div>
+    <div className=' w-full h-full flex items-center text-4xl  justify-center bg-[#f2f2f2] text-black'>Home</div>
   )
 }
 
